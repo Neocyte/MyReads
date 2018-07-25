6635e988a0053a32e4c8f1ec3f7cbbd44b1b2167
Mason Wu <masonwu8462@yahoo.com>
2018-07-25 15:22:57 -0400

    Feat: Add search page properties

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -3,4 +3,23 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Book from './Book';
 
+class Search extends React.Component {
+  propTypes = {
+    books: PropTypes.array.isRequired,
+    changeShelf: PropTypes.func.isRequired
+  };
+
+  updateSearch = (search) => {
+    this.props.updateSearch(search.trim());
+  };
+
+  componentWillUnmount() {
+    this.props.updateSearch('');
+  }
+
+  render() {
+    
+  }
+}
+
 export default SearchPage;
