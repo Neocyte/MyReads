d429810d7c5775e77bdcb3d8a5cb0a171b6cb82c
Mason Wu <masonwu8462@yahoo.com>
2018-07-25 15:25:23 -0400

    Feat: Add bookshelf properties

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -2,4 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Book from './Book';
 
+class BookShelf extends React.Component {
+  propTypes = {
+    title: PropTypes.string.isRequired,
+    books: PropTypes.array.isRequired,
+    changeShelf: PropTypes.func.isRequired
+  };
+
+  render() {
+    
+  }
+}
+
 export default BookShelf;
