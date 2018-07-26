c1b6ccf995a9c233811bd4b44e424465c198aed3
Mason Wu <masonwu8462@yahoo.com>
2018-07-26 16:13:03 -0400

    Refactor: Split render()'s JSX to each component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -11,7 +11,13 @@ class BookShelf extends React.Component {
 
   render() {
     return (
-      
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">Want to Read</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+          </ol>
+        </div>
+      </div>
     )
   }
 }
