beffbb9273a24a8fd579e7042d996b39fbd77512
Mason Wu <masonwu8462@yahoo.com>
2018-07-26 16:36:13 -0400

    Feat: Add Book component to SearchPage.js render()

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -24,20 +24,24 @@ class Search extends React.Component {
         <div className="search-books-bar">
           <Link className="close-search" to="/">Close</Link>
           <div className="search-books-input-wrapper">
-            {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
-            <input type="text" placeholder="Search by title or author"/>
-
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              onChange={(event) => this.updateSearch(event.target.value)}
+            />
           </div>
         </div>
         <div className="search-books-results">
           <ol className="books-grid"></ol>
+            {this.props.books.map((book) => (
+              <li key={book.id} className="contact-list-item">
+                <Book
+                  book={book}
+                  changeShelf={this.props.changeShelf}
+                />
+              </li>
+            ))}
+          </ol>
         </div>
       </div>
     )
