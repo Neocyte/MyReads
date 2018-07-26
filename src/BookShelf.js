8382fbb2a31e1ef1a236afce03933f76d9421e4b
Mason Wu <masonwu8462@yahoo.com>
2018-07-26 16:38:29 -0400

    Feat: Add Book component to BookShelf.js render()

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -12,9 +12,16 @@ class BookShelf extends React.Component {
   render() {
     return (
       <div className="bookshelf">
-        <h2 className="bookshelf-title">Want to Read</h2>
+        <h2 className="bookshelf-title">{this.props.title}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
+            {this.props.books.map((book) => (
+              <li key={book.id} className="contact-list-item">
+                <Book
+                  book={book}
+                  changeShelf={this.props.changeShelf}/>
+              </li>
+            ))}
           </ol>
         </div>
       </div>
